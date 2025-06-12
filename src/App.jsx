a99b625ac11349e14db814955d7ffbd06fb559a0
Kapil Pal <kpal4863@gmailcom>
2025-06-12 21:31:26 +0530

    install react js and talwind css create first template

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Profile from "./Pages/Profile"
+import Signin from "./Pages/Signin"
+import Signup from "./Pages/Signup"
 
 function App() {
   return (
-    <div className='text-red-600'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
